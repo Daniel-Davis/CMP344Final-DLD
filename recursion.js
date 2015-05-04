f369db2df27bb43557f1947c81fa83260daeb529
Daniel-Davis <ddv9461@saintjoe.edu>
2015-05-04 13:57:13 -0500

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,2 +1,8 @@
 //Daniel Davis, recursion, takes two numbers and adds 'X' to itself 'Y' amount of times//
 
+function addSelf(num1,num2) { // recursive code that takes two numbers
+  if (num1 === 0) { // if the first number is 0
+    return num2; // then return the second number
+  }// end of if statement
+  return = addSelf(num1 - 1, num2 + num2); // decrease the first number by 1, and add the second number to itself
+}// end of function
